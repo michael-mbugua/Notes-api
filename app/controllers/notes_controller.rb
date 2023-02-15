92550d4baf3a3a2d3c72bf4ae0ab959b7b60beaa
michael-mbugua <kamirimichael369@gmail.com>
2023-02-15 23:38:47 +0300

    add all the controller actions CRUD operations

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,27 @@
 class NotesController < ApplicationController
+    # create an end point to show all available notes
+    def index
+        notes=Note.all;
+        render json: notes , status: :ok
+    end
+    def show
+        notes=Note.find(params[:id])
+        render json:notes
+    end
+    def create
+        notes=Note.create(notes_params)
+        render json: notes, status: :created
+    end
+    def update
+        notes=Note.find(params[:id])
+        notes.update(notes_params)
+        render json: notes
+    end
+    def destroy
+        notes=Note.find(params[:id])
+        notes.destroy
+    end
+    def notes_params
+        params.permit(:notes)
+    end
 end

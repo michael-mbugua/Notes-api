4c73119b1900c4ddc632136adf8454cfe255a156
michael-mbugua <kamirimichael369@gmail.com>
2023-02-15 23:40:51 +0300

    add comments explaining the code

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,23 +4,33 @@ class NotesController < ApplicationController
         notes=Note.all;
         render json: notes , status: :ok
     end
+
+# READ  controller action
     def show
         notes=Note.find(params[:id])
         render json:notes
     end
+
+# CREATE controller action
     def create
         notes=Note.create(notes_params)
         render json: notes, status: :created
     end
+
+# UPDATE controller action
     def update
         notes=Note.find(params[:id])
         notes.update(notes_params)
         render json: notes
     end
+
+# DELETE controller action
     def destroy
         notes=Note.find(params[:id])
         notes.destroy
     end
+
+    private
     def notes_params
         params.permit(:notes)
     end

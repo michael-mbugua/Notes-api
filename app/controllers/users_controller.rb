6ddbbf8c7f45586729ac8b205ac980f0c1bfe824
michael-mbugua <kamirimichael369@gmail.com>
2023-02-16 22:51:27 +0300

    controller action to enable users to be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    # controller action to enable users to be created
+    def create
+        user=User.create(user_params)
+        if user.valid?
+            render json: {user: UserSerializer.new(user)},status: :created
+        else
+            render json: {error: 'failed to create user'},status: :unprocessable_entity
+        end
+    end
+
+    # entities that aren't available to everyone
+    private
+    def user_params
+        params.permit(:username,:email,:password)
+    end
 end

6172ae2dddb27f21cba47f12e1fee71706372f01
michael-mbugua <kamirimichael369@gmail.com>
2023-02-17 13:07:23 +0300

    add custom routing to login and profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :notes
   resource :users, only: [:create]
-  post '/login', to: 'auth#create'
+  post 'users/login', to: 'auth#create'
   get '/profile', to: 'user#profile'
 end
